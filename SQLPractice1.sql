217a99edda0d5312dd98ec6e7dbfb5ea99c00952
maxmanish <max.manish02@gmail.com>
2026-04-16 14:12:42 +0530

    new file updated

--- SQLPractice1.sql
+++ SQLPractice1.sql
@@ -58,4 +58,51 @@ select emp_name from employees where salary in
 select emp_name from employees where salary = (select (max(salary) from employees));
 
 -- Write a SQL DDL statement to create a table orders
-create table orders (order_id INT primary key, customer_id INT NOT NULL, order_date DATE, amount INT(10) check (amount > 0));
+create table orders (order_id INT primary key, customer_id INT NOT NULL, order_date DATE, amount INT check (amount > 0));
+
+-- Write a SQL query to create a table students
+create table students (student_id INT primary key, email varchar2(100) unique, 
+    age int check (age>=18), created_date DATE default (CURRENT_DATE));
+
+-- Write a SQL query to increase the price by 10% for all products belonging to the category 'Electronics'
+update products set price=price*(1.1)
+where category = 'Electronics';
+
+-- Write a SQL query to find customers who have NOT placed any orders
+select customer_id from customers c
+left outer join orders o
+on c.customer_id = o.customer_id
+and o.order_id is NULL;
+
+select customer_id from customer c
+where not exists (select * from orders o where c.customer_id = o.customer_id)
+
+-- Write a SQL query to delete all login records except the most recent login for each user
+-- logins(user_id, login_date)
+delete from logins l1 where login_date < 
+    (select max(login_date) from logins l2 where l1.user_id = l2.user_id);
+
+delete from logins (
+    select *, row_number() over(partition by user_id order by login_date desc) as latest_login
+) where latest_login > 1;
+
+-- Write a SQL query to add a constraint (also drop the constraint) that ensures email values are UNIQUE
+alter table employees add constraint unq_emp_mail unique(email); --Oracle
+alter table employees add unique(email); --MySQL
+alter table employees drop constraint unq_emp_mail;
+
+-- Write a SQL query to find all orders placed in the last 7 days
+select order_id from orders where order_date > (sysdate-7);
+
+-- Write a SQL query to find customers who have placed more than one order AND whose total purchase amount is greater than 10K
+select customer_id from sales group by customer_id having sum(amount) > 10000 and count(order_id) > 1;
+
+-- Write a SQL query to find departments where every employee earns more than 50,000
+select dept_id from employees group by emp_id having sum(salary) > 50000; --incorrect
+select dept_id from employees group by dept_id having min(salary) > 50000; --correct
+
+-- Write a SQL query to find customers who have placed exactly ONE order
+select customer_id from orders group by customer_id having count(order_id) = 1;
+
+-- Write a SQL query to find departments that have at least one employee but fewer than 3 employees
+select dept_id from employees group by dept_id having count(emp_id) > 1 and count(emp_id) < 3;
